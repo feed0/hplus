e0d547b79128e75c3e34d54be122c647074ac703
feed0 <dogmas-gutters.07@icloud.com>
2026-03-11 08:35:51 -0300

    [GQLSB-03] Creating a Spring Data repository - ADDED:     - Customer.java;     - CustomerRepository.java;     - data.sql;     - schema.sql.
    - MODIFIED:
        - application.properties: dll-auto=none (DML only)

---
+++ src/main/java/com/feed0/hplus/data/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.feed0.hplus.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findCustomerByEmail(String email);
+}
